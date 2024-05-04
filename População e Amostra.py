fc0097375463e24adc5d3f063754d60308cc85d4
Kennedy Anderson <keke.218@gmail.com>
2024-05-04 09:41:27 -0300

    Update População e Amostra.py

--- População e Amostra.py
+++ População e Amostra.py
@@ -128,3 +128,34 @@ for x, y in split.split(dataset, dataset['income']):
 
 df_x.shape, df_y.shape
 
+
+# Amostagem de reservatório
+
+stream = []
+for i in range(len(dataset)):
+    stream.append(i)
+
+def amostragem_reservatorio(dataset, amostras):
+    stream = []
+    for i in range(len(dataset)):
+        stream.append(i)
+    
+    i = 0
+    tamanho = len(dataset)
+    reservatorio = [0] * amostras
+    for i in range(amostras):
+        reservatorio[i] = stream[i]
+    
+    while i < tamanho:
+        j = random.randrange(i + 1)
+        if j < amostras:
+            reservatorio[j] = stream[i]
+        i += 1
+    
+    return dataset.iloc[reservatorio]
+
+df_amostragem_reservatorio = amostragem_reservatorio(dataset, 1000)
+
+df_amostragem_reservatorio.shape
+
+df_amostragem_reservatorio.head()
