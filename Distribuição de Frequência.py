d644be9ca0786efe05634df0437c3e21cbac6cd2
Kennedy Anderson <keke.218@gmail.com>
2024-05-06 15:50:02 -0300

    Update Distribuição de Frequência.py

--- Distribuição de Frequência.py
+++ Distribuição de Frequência.py
@@ -106,4 +106,18 @@ dataset.plot.hist();
 sns.displot(dados, kde = True);
 
 
+# Exercicio 1
+
+dataset = pd.read_csv('Bases de dados/census.csv')
+
+dataset.head()
+
+dataset['age'].max()
+dataset['age'].min()
+
+dataset['age'].plot.hist();
+
+dataset['Faixa-Etária'] = pd.cut(dataset['age'], bins=[0, 17, 25, 40, 60, 90], labels = ['Faixa1', 'Faixa2', 'Faixa3', 'Faixa4', 'Faixa5'])
+
+dataset.head()
 
