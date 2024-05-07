d57ef595f0d1544e612dab8a77eeafc48c409b38
Kennedy Anderson <keke.218@gmail.com>
2024-05-06 22:21:50 -0300

    Update Medidas de Posição e Dispersão.py

--- Medidas de Posição e Dispersão.py
+++ Medidas de Posição e Dispersão.py
@@ -44,5 +44,114 @@ np.median(dados)
 
 statistics.median(dados)
 
+# Média aritmética ponderada
 
+notas = np.array([9, 8, 7, 3])
+pesos = np.array([1, 2, 3, 4])
+
+media_ponderada = (notas * pesos).sum() / pesos.sum()
+
+np.average(notas, weights=pesos)
+
+# Média aritmética, moda e mediana com distribuição de frequência (Dados agrupados)
+
+dados = {'inferior': [150, 154, 158, 162, 166, 170],
+         'superior': [154, 158, 162, 166, 170, 174],
+         'fi': [5, 9, 11, 7, 5, 3]}
+
+dataset = pd.DataFrame(dados)
+dataset
+
+dataset['xi'] = (dataset['superior'] + dataset['inferior']) / 2
+
+dataset
+
+dataset['fi.xi'] = dataset['fi'] * dataset['xi']
+
+dataset
+
+dataset['Fi'] = 0
+
+
+frequencia_acumulada = []
+somatorio = 0
+for linha in dataset.iterrows():
+    somatorio += linha[1][2]
+    frequencia_acumulada.append(somatorio)
+
+frequencia_acumulada
+
+dataset['Fi'] = frequencia_acumulada
+
+dataset
+
+# Média
+
+media = dataset['fi.xi'].sum() / dataset['fi'].sum()
+
+media
+
+# Moda
+
+dataset[dataset['fi'] == dataset['fi'].max()]
+
+dataset[dataset['fi'] == dataset['fi'].max()]['xi'].values[0]
+
+# Mediana
+
+fi_2 = dataset['fi'].sum() / 2
+
+fi_2
+
+
+limite_inferior, frequencia_classes, id_frequencia_anterior = 0, 0, 0
+for linha in dataset.iterrows():
+    limite_inferior = linha[1][0]
+    frequencia_classes = linha[1][2]
+    id_frequencia_anterior = linha[0]
+    if linha[1][5] >= fi_2:
+        id_frequencia_anterior -= 1
+        break
+
+limite_inferior, frequencia_classes, id_frequencia_anterior
+
+Fi_anterior = dataset.iloc[[id_frequencia_anterior]]['Fi'].values[0]
+
+mediana = limite_inferior + ((fi_2 - Fi_anterior) * 4) / frequencia_classes
+
+
+def get_estatisticas(dados):
+    media = dados['fi.xi'].sum() / dados['fi'].sum()
+    moda = dados[dados['fi'] == dados['fi'].max()]['xi'].values[0]
+
+    fi_2 = dados['fi'].sum() / 2
+
+    limite_inferior, frequencia_classes, id_frequencia_anterior = 0, 0, 0
+    for linha in dados.iterrows():
+        limite_inferior = linha[1][0]
+        frequencia_classes = linha[1][2]
+        id_frequencia_anterior = linha[0]
+        if linha[1][5] >= fi_2:
+            id_frequencia_anterior -= 1
+            break
+    Fi_anterior = dados.iloc[[id_frequencia_anterior]]['Fi'].values[0]
+    mediana = limite_inferior + ((fi_2 - Fi_anterior) * 4) / frequencia_classes
+
+    return media, moda, mediana
+
+get_estatisticas(dataset)
+
+# Média geométrica
+
+from scipy.stats.mstats import gmean, hmean
+
+gmean([2, 3, 4])
+
+# Média harmônica
+
+hmean([2, 3, 4])
+
+# Média quadrática
+
+np.sqrt(np.square([2, 3, 4]).sum() / len([2, 3, 4]))
 
