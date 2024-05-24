dad710bd7278ff43c6a81f05d4d803123530b08c
Kennedy Anderson <keke.218@gmail.com>
2024-05-24 18:13:21 -0300

    Update Correlação e Regressão.py

--- Correlação e Regressão.py
+++ Correlação e Regressão.py
@@ -134,3 +134,125 @@ regressor.score(x_treino, y_treino)
 
 regressor.score(x_teste, y_teste)
 
+
+# Métricas de erros
+
+previsoes = regressor.predict(x_teste)
+
+previsoes, y_teste
+
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+mean_absolute_error(y_teste, previsoes)
+
+mean_squared_error(y_teste, previsoes)
+
+math.sqrt(mean_squared_error(y_teste, previsoes)) # RMSE
+
+
+# Regressão Linear Múltipla
+
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots(figsize=(15,15))
+ax = sns.heatmap(dataset.corr(), annot=True)
+
+x = dataset.iloc[:, [2,3,9,10]].values
+
+y = dataset.iloc[:, 0].values
+
+
+from sklearn.model_selection import train_test_split
+
+x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size = 0.2, random_state=1)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_treino, y_treino)
+
+regressor.score(x_treino, y_treino)
+
+regressor.score(x_teste, y_teste)
+
+previsoes = regressor.predict(x_teste)
+mean_absolute_error(y_teste, previsoes)
+
+import matplotlib.pyplot as plt
+f, ax = plt.subplots(2, 2, figsize=(10, 10))  # Cria uma matriz 2x2 de gráficos
+ax[0, 0].hist(x[0])
+ax[0, 1].hist(x[1])
+ax[1, 0].hist(x[2])
+ax[1, 1].hist(x[3])
+plt.show()
+
+
+plt.hist(y);
+
+y = np.log1p(y)
+
+plt.hist(y);
+
+
+from sklearn.model_selection import train_test_split
+
+x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size = 0.2, random_state=1)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_treino, y_treino)
+
+regressor.score(x_treino, y_treino)
+
+regressor.score(x_teste, y_teste)
+
+previsoes = regressor.predict(x_teste)
+mean_absolute_error(y_teste, previsoes)
+
+
+# EXERCICIOS
+dataset = pd.read_csv('Bases de dados/house_prices.csv')
+dataset.head()
+dataset.shape
+
+dataset.drop(labels = ['sqft_living15', 'sqft_lot15'], axis = 1, inplace = True)
+
+x = dataset.iloc[:, 1:17].values
+y = dataset.iloc[:, 0].values
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+regressor = LinearRegression()
+regressor.fit(x_treino, y_treino)
+
+regressor.score(x_treino, y_treino)
+
+regressor.score(x_teste, y_teste)
+
+
+# Seleção de atributos
+
+from sklearn.feature_selection import SelectFdr, f_regression
+
+selecao = SelectFdr(f_regression, alpha = 0.0)
+
+X_novo = selecao.fit_transform(x, y)
+x.shape, X_novo.shape
+
+selecao.pvalues_
+
+colunas = selecao.get_support()
+colunas
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+x_treino, x_teste, y_treino, y_teste = train_test_split(X_novo, y, test_size = 0.2, random_state = 0)
+
+regressor = LinearRegression()
+regressor.fit(x_treino, y_treino)
+
+regressor.score(x_treino, y_treino)
+
+regressor.score(x_teste, y_teste)
